2ac6695e804adae063998576f54f694d61a29a11
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-09 13:14:40 +0530

    chore(neo-brut) : all the junk that came with the previous installation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import Page from "./pages/home/home-page.tsx";
+
 function App() {
   return (
-    <div className="min-h-screen bg-yellow-200 py-12 px-4 sm:px-6 lg:px-8">
-      Hello !!!
+    <div className="min-h-screen">
+      <Page />
     </div>
   );
 }
